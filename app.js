b7a6b85ca9ba980481ef94d38097c3bc0ef92ad1
Kalpana <kalpana@Kalpanas-MacBook-Pro.local>
2021-01-19 19:18:58 +0530

    Project

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
+app.get("/",function(req,res){
+  res.render("home");
+});
 
 
 
